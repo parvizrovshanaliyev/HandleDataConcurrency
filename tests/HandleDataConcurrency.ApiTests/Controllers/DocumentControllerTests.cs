8a69af7fd249ab2865fa3b7d8808bd15765c5b72
Parviz Rovshan Aliyev <44087592+parvizrovshanaliyev@users.noreply.github.com>
2024-02-01 15:47:56 +0400

    GivenDocumentEndpoint_WhenConcurrentDocumentCreation_ThenOk

--- tests/HandleDataConcurrency.ApiTests/Controllers/DocumentControllerTests.cs
+++ tests/HandleDataConcurrency.ApiTests/Controllers/DocumentControllerTests.cs
@@ -18,18 +18,15 @@ public class DocumentControllerTests : IClassFixture<CustomWebApplicationFactory
     }
 
     /// <summary>
-    /// simultaneously obtaining the document number and testing Concurrency Conflicts handling
+    /// Test method for concurrently obtaining document numbers and testing Concurrency Conflicts handling.
     /// </summary>
     [Fact]
-    public async Task GivenDocumentEndpoint_WhenGetDocumentNumber_ThenOk()
+    public async Task GivenDocumentEndpoint_WhenConcurrentDocumentCreation_ThenOk()
     {
-        using var client1 = _factory
-            .CreateClient();
+        using var client1 = _factory.CreateClient();
+        using var client2 = _factory.CreateClient();
 
-        using var client2 = _factory
-            .CreateClient();
-
-        // use Task.WhenAll to run multiple tasks concurrently
+        // Use Task.WhenAll to run multiple tasks concurrently
         var tasks = new List<Task<HttpResponseMessage>>
         {
             client1.PostAsync("api/document/create-document", new StringContent(string.Empty)),
@@ -43,7 +40,7 @@ public class DocumentControllerTests : IClassFixture<CustomWebApplicationFactory
 
         var document1 = await responses[0].ReadAndAssertSuccessAsync<Document>();
         var document2 = await responses[1].ReadAndAssertSuccessAsync<Document>();
-        
+
         document1.Number.Should().NotBe(document2.Number);
     }
 }
